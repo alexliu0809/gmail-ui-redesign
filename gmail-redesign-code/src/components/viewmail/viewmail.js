d2f208a86f9e4422be5dbea585b10fb0ca9dc9fe
Alex <alexliu0809@hotmail.com>
2022-04-03 17:29:52 -0700

    More style for arrow down

--- gmail-redesign-code/src/components/viewmail/viewmail.js
+++ gmail-redesign-code/src/components/viewmail/viewmail.js
@@ -1,5 +1,5 @@
-import { Avatar, Button, Checkbox } from "@material-ui/core";
-import { MoreVert, Refresh, ArrowBack, ArrowDropDown } from "@material-ui/icons";
+import { Avatar, Button, Checkbox, makeStyles } from "@material-ui/core";
+import { MoreVert, Refresh, ArrowBack, ArrowDropDown, Block } from "@material-ui/icons";
 import React from "react";
 import { useLocalContext } from "../../context/context";
 import "./styles.css";
@@ -14,6 +14,21 @@ const ViewMail = ({ mailState }) => {
     let domain = email.split('@').pop()
     return domain
   }
+
+  const useStyles = makeStyles({
+    myArrowDropDown: {
+      "vertical-align": "bottom",
+      "fontSize": '1.0rem',
+      "margin-bottom": "0.1rem",
+      "cursor": "pointer",
+
+      '&:hover': {
+        "background": "#f2f2f2;",
+        "margin-bottom": "0.1rem",
+      },
+  }})
+
+  const classes = useStyles();
 
   return (
     <div className={`main ${!drawerOpen && "main--fullWidth"}`}>
@@ -34,19 +49,28 @@ const ViewMail = ({ mailState }) => {
               <h1 className="viewMail__senderName">{mailState.state.from_name}</h1>
               <p className="viewMail__senderMail">{
               `<${mailState.state.from}>`} 
+              </p> 
               { getDomainFromEmail(mailState.state.from) === getDomainFromEmail(mailState.state.mailfrom)
                 ? ("") : (
-                    ` via ${getDomainFromEmail(mailState.state.mailfrom)}`
+                  <p className="viewMail__senderMail">
+                    &nbsp;
+                  <a className="viewMail__senderMail viewMail__senderMailVia">via</a>
+                    &nbsp;
+                    {getDomainFromEmail(mailState.state.mailfrom)}
+                  </p>
+                  
                 )
               }
-              </p> 
+
               
             </div>
             <p className="viewMail__info">to {
               mailState.state.participant_email === mailState.state.to ? ("me") : (`${mailState.state.to}`)
             }
-            <ArrowDropDown style={{ fontSize: '1.0rem' }}/>
+              <ArrowDropDown className={classes.myArrowDropDown}/>
             </p>
+
+            
             
           </div>
         </div>
