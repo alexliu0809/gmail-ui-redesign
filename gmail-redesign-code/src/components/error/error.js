e64009427a21ebbd7fe1df5d991f5796fbe7bf8d
Alex <alexliu0809@hotmail.com>
2022-05-02 13:03:14 -0700

    Error code

--- gmail-redesign-code/src/components/error/error.js
+++ gmail-redesign-code/src/components/error/error.js
@@ -112,9 +112,12 @@ export const ErrorScreenTooSmaller = ({errorMessage=null, helpMessage=null}) => 
             <br />
             <br />
             <h2>
-                Please use a laptop or a desktop with a bigger screen. <br></br>
-                The resolution of your screen (or current window) should be at least <span style={{"color":"blue","font-weight":"bold"}}>1024 * 600</span>. <br></br>
-                Please contact Alex Liu at e7liu@eng.ucsd.edu for any questions.
+            Please use a laptop or a desktop with a bigger screen. If you are already on a laptop or desktop with a large screen, please maximize your browser size. You can drag a corner of the browser to a larger size, and then refresh the page. You can also try the directions below to enlarge the browser. <br></br>
+            <br></br>
+            <span style={{"color":"blue","font-weight":"bold"}}>Mac Users</span>: On your Mac, do the following to maximize a window: Press and hold the Option key while you click the green button in the top-left corner of an app window. Then refresh the page. <br></br> (from <a href='https://support.apple.com' target="_blank">support.apple.com</a>)<br></br><br></br>
+            <span style={{"color":"blue","font-weight":"bold"}}>PC Users</span>: On your PC, do the following to maximize a window: Press the square button at the top right corner of the browser. You can also Press F11 or Fn and F11 at the same time. Then refresh the page. <br></br>(from <a href='https://computerhope.com' target="_blank">computerhope.com</a>) <br></br><br></br>
+
+            If you try the directions mentioned above and still have an issue, please send a message to Alex Liu at e7liu@eng.ucsd.edu for any questions.
             </h2>
         </div>
     )
